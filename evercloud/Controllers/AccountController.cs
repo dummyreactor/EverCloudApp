792a61d29158e7652f22cf08c4c2f490eae85129
dummyreactor <dummyreactor@gmail.com>
2025-07-10 17:25:46 +0530

    async login

--- evercloud/Controllers/AccountController.cs
+++ evercloud/Controllers/AccountController.cs
@@ -1,170 +1,4 @@
-﻿//using evercloud.Service.Interfaces;
-//using evercloud.ViewModels;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-
-//namespace evercloud.Controllers
-//{
-//    public class AccountController : Controller
-//    {
-//        private readonly SignInManager<Users> signInManager;
-//        private readonly IAccountService accountService;
-
-//        public AccountController(SignInManager<Users> signInManager, IAccountService accountService)
-//        {
-//            this.signInManager = signInManager;
-//            this.accountService = accountService;
-//        }
-
-//        public IActionResult Login()
-//        {
-//            return View();
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> Login(LoginViewModel model)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
-
-//                if (result.Succeeded)
-//                {
-//                    return RedirectToAction("Index", "Home");
-//                }
-
-//                ModelState.AddModelError("", "Email or password is incorrect.");
-//            }
-
-//            return View(model);
-//        }
-
-//        public IActionResult Register()
-//        {
-//            return View();
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> Register(RegisterViewModel model)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                Users user = new Users
-//                {
-//                    FullName = model.Name,
-//                    Email = model.Email,
-//                    UserName = model.Email,
-//                };
-
-//                bool success = await accountService.RegisterUserAsync(user, model.Password);
-
-//                if (success)
-//                    return RedirectToAction("Login", "Account");
-
-//                ModelState.AddModelError("", "Failed to create account.");
-//            }
-
-//            return View(model);
-//        }
-
-//        public IActionResult VerifyEmail()
-//        {
-//            return View();
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> VerifyEmail(VerifyEmailViewModel model)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                var user = await accountService.FindByUsernameAsync(model.Email);
-
-//                if (user == null)
-//                {
-//                    ModelState.AddModelError("", "Something is wrong!");
-//                }
-//                else
-//                {
-//                    return RedirectToAction("ChangePassword", "Account", new { username = user.UserName });
-//                }
-//            }
-
-//            return View(model);
-//        }
-
-//        public IActionResult ChangePassword(string username)
-//        {
-//            if (string.IsNullOrEmpty(username))
-//            {
-//                return RedirectToAction("VerifyEmail", "Account");
-//            }
-
-//            return View(new ChangePasswordViewModel { Email = username });
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                var user = await accountService.FindByUsernameAsync(model.Email);
-//                if (user != null)
-//                {
-//                    bool result = await accountService.ResetPasswordAsync(user, model.NewPassword);
-
-//                    if (result)
-//                        return RedirectToAction("Login", "Account");
-
-//                    ModelState.AddModelError("", "Failed to reset password.");
-//                }
-//                else
-//                {
-//                    ModelState.AddModelError("", "Email not found!");
-//                }
-//            }
-//            else
-//            {
-//                ModelState.AddModelError("", "Something went wrong. try again.");
-//            }
-
-//            return View(model);
-//        }
-
-//        public async Task<IActionResult> Logout()
-//        {
-//            await signInManager.SignOutAsync();
-//            return RedirectToAction("Index", "Home");
-//        }
-
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public async Task<IActionResult> DeleteAccount()
-//        {
-//            var user = await signInManager.UserManager.GetUserAsync(User);
-
-//            if (user == null)
-//            {
-//                TempData["Error"] = "User not found.";
-//                return RedirectToAction("Dashboard", "Account");
-//            }
-
-//            bool result = await accountService.DeleteAccountAsync(user);
-
-//            if (result)
-//            {
-//                await signInManager.SignOutAsync();
-//                TempData["Message"] = "Your account has been deleted successfully.";
-//                return RedirectToAction("Index", "Home");
-//            }
-
-//            TempData["Error"] = "Failed to delete the account.";
-//            return RedirectToAction("Dashboard", "Account");
-//        }
-//    }
-//}
-
-
-using evercloud.Domain.Interfaces;
+﻿using evercloud.Domain.Interfaces;
 using evercloud.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -173,8 +7,14 @@ namespace evercloud.Controllers
 {
     public class AccountController(SignInManager<Users> signInManager, IAccountService accountService) : Controller
     {
-        private readonly SignInManager<Users> signInManager = signInManager;
-        private readonly IAccountService accountService = accountService;
+        //private readonly SignInManager<Users> signInManager;
+        //private readonly IAccountService accountService;
+
+        //public AccountController(SignInManager<Users> signInManager, IAccountService accountService)
+        //{
+        //    this.signInManager = signInManager;
+        //    this.accountService = accountService;
+        //}
 
         public IActionResult Login()
         {
@@ -182,11 +22,11 @@ namespace evercloud.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var result = signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false).Result;
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
 
                 if (result.Succeeded)
                 {
@@ -205,18 +45,18 @@ namespace evercloud.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                Users user = new()
+                Users user = new ()
                 {
                     FullName = model.Name,
                     Email = model.Email,
                     UserName = model.Email,
                 };
 
-                bool success = accountService.RegisterUser(user, model.Password);
+                bool success = await accountService.RegisterUserAsync(user, model.Password);
 
                 if (success)
                     return RedirectToAction("Login", "Account");
@@ -233,11 +73,11 @@ namespace evercloud.Controllers
         }
 
         [HttpPost]
-        public IActionResult VerifyEmail(VerifyEmailViewModel model)
+        public async Task<IActionResult> VerifyEmail(VerifyEmailViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var user = accountService.FindByUsername(model.Email);
+                var user = await accountService.FindByUsernameAsync(model.Email);
 
                 if (user == null)
                 {
@@ -263,14 +103,14 @@ namespace evercloud.Controllers
         }
 
         [HttpPost]
-        public IActionResult ChangePassword(ChangePasswordViewModel model)
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var user = accountService.FindByUsername(model.Email);
+                var user = await accountService.FindByUsernameAsync(model.Email);
                 if (user != null)
                 {
-                    bool result = accountService.ResetPassword(user, model.NewPassword);
+                    bool result = await accountService.ResetPasswordAsync(user, model.NewPassword);
 
                     if (result)
                         return RedirectToAction("Login", "Account");
@@ -290,17 +130,17 @@ namespace evercloud.Controllers
             return View(model);
         }
 
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            signInManager.SignOutAsync().Wait();
+            await signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult DeleteAccount()
+        public async Task<IActionResult> DeleteAccount()
         {
-            var user = signInManager.UserManager.GetUserAsync(User).Result;
+            var user = await signInManager.UserManager.GetUserAsync(User);
 
             if (user == null)
             {
@@ -308,11 +148,11 @@ namespace evercloud.Controllers
                 return RedirectToAction("Dashboard", "Account");
             }
 
-            bool result = accountService.DeleteAccount(user);
+            bool result = await accountService.DeleteAccountAsync(user);
 
             if (result)
             {
-                signInManager.SignOutAsync().Wait();
+                await signInManager.SignOutAsync();
                 TempData["Message"] = "Your account has been deleted successfully.";
                 return RedirectToAction("Index", "Home");
             }
@@ -322,3 +162,160 @@ namespace evercloud.Controllers
         }
     }
 }
+
+
+//using evercloud.Domain.Interfaces;
+//using evercloud.ViewModels;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace evercloud.Controllers
+//{
+//    public class AccountController(SignInManager<Users> signInManager, IAccountService accountService) : Controller
+//    {
+//        public IActionResult Login()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public IActionResult Login(LoginViewModel model)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                var result = signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false).Result;
+
+//                if (result.Succeeded)
+//                {
+//                    return RedirectToAction("Index", "Home");
+//                }
+
+//                ModelState.AddModelError("", "Email or password is incorrect.");
+//            }
+
+//            return View(model);
+//        }
+
+//        public IActionResult Register()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public IActionResult Register(RegisterViewModel model)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                Users user = new()
+//                {
+//                    FullName = model.Name,
+//                    Email = model.Email,
+//                    UserName = model.Email,
+//                };
+
+//                bool success = accountService.RegisterUser(user, model.Password);
+
+//                if (success)
+//                    return RedirectToAction("Login", "Account");
+
+//                ModelState.AddModelError("", "Failed to create account.");
+//            }
+
+//            return View(model);
+//        }
+
+//        public IActionResult VerifyEmail()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public IActionResult VerifyEmail(VerifyEmailViewModel model)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                var user = accountService.FindByUsername(model.Email);
+
+//                if (user == null)
+//                {
+//                    ModelState.AddModelError("", "Something is wrong!");
+//                }
+//                else
+//                {
+//                    return RedirectToAction("ChangePassword", "Account", new { username = user.UserName });
+//                }
+//            }
+
+//            return View(model);
+//        }
+
+//        public IActionResult ChangePassword(string username)
+//        {
+//            if (string.IsNullOrEmpty(username))
+//            {
+//                return RedirectToAction("VerifyEmail", "Account");
+//            }
+
+//            return View(new ChangePasswordViewModel { Email = username });
+//        }
+
+//        [HttpPost]
+//        public IActionResult ChangePassword(ChangePasswordViewModel model)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                var user = accountService.FindByUsername(model.Email);
+//                if (user != null)
+//                {
+//                    bool result = accountService.ResetPassword(user, model.NewPassword);
+
+//                    if (result)
+//                        return RedirectToAction("Login", "Account");
+
+//                    ModelState.AddModelError("", "Failed to reset password.");
+//                }
+//                else
+//                {
+//                    ModelState.AddModelError("", "Email not found!");
+//                }
+//            }
+//            else
+//            {
+//                ModelState.AddModelError("", "Something went wrong. try again.");
+//            }
+
+//            return View(model);
+//        }
+
+//        public IActionResult Logout()
+//        {
+//            signInManager.SignOutAsync().Wait();
+//            return RedirectToAction("Index", "Home");
+//        }
+
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public IActionResult DeleteAccount()
+//        {
+//            var user = signInManager.UserManager.GetUserAsync(User).Result;
+
+//            if (user == null)
+//            {
+//                TempData["Error"] = "User not found.";
+//                return RedirectToAction("Dashboard", "Account");
+//            }
+
+//            bool result = accountService.DeleteAccount(user);
+
+//            if (result)
+//            {
+//                signInManager.SignOutAsync().Wait();
+//                TempData["Message"] = "Your account has been deleted successfully.";
+//                return RedirectToAction("Index", "Home");
+//            }
+
+//            TempData["Error"] = "Failed to delete the account.";
+//            return RedirectToAction("Dashboard", "Account");
+//        }
+//    }
+//}
